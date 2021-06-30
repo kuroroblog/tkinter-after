327f07511ae8565dce094284d32b245f3f1e18db
tocchimittsu <arimagedon21@gmail.com>
2021-07-01 08:58:46 +0900

    修正

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ class Application(tk.Frame):
     # keyword : dict型
     def getHelloWorld(self, keyword):
         print(keyword['text'])
+        # 5000ms後にself.getHelloWorld関数を実行
+        # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
+        # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
+        # 第三引数 : 関数実行時に渡される引数
+        # 戻り値 : ID(after関数の設定情報に紐づく文字列)
         # self.master.after(5000, self.getHelloWorld, {
         #     "text" : "hello world",
         # })
 
     # after関数に関するサンプル関数
     def getAfterSample(self):
-        # 5000ms後にgetHelloWorld関数を実行
+        # 5000ms後にself.getHelloWorld関数を実行
+        # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
+        # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
+        # 第三引数 : 関数実行時に渡される引数
+        # 戻り値 : ID(after関数の設定情報に紐づく文字列)
         return self.master.after(5000, self.getHelloWorld, {
             "text" : "hello world",
         })
@@ -25,20 +34,25 @@ class Application(tk.Frame):
 
     # time.sleepとafter関数を比較する関数
     def confirmTimeVsAfter(self):
+        # Windowを親要素としてbutton Widgetを作成する。
+        # command : ボタンをクリックした場合に、実行する関数を設定。self.getHelloとする。
+        # text : テキスト情報
         # Buttonについて : https://kuroro.blog/python/oFju6EngDtcYtIiMIDf1/
-        button = tk.Button(command=self.getHello)
+        button = tk.Button(self.master, command=self.getHello, text='クリック')
+        # Windowを親要素として、button Widgetをどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         button.pack()
 
     # after関数をキャンセルする関数
-    # id : after関数に紐づくid
+    # id : after関数の設定情報に紐づく文字列
     def removeAfterSample(self, id):
+        # 第一引数 : after関数の設定情報に紐づく文字列
         self.master.after_cancel(id)
 
     def __init__(self, master=None):
         # Windowの初期設定を行う。
         super().__init__(master)
-        # アプリ画面(Window)の位置、大きさを設定する。
+        # Windowの画面サイズを設定する。
         # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
         self.master.geometry("300x200+120+0")
 
