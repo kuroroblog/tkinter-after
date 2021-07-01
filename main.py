39a8f844c887c5a23186eb66b5a9160e93e76765
tocchimittsu <arimagedon21@gmail.com>
2021-07-01 09:13:33 +0900

    修正

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Application(tk.Frame):
         # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
         # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
         # 第三引数(任意) : 関数実行時に渡される引数
-        # 戻り値 : ID(after関数の設定情報に紐づく文字列)
+        # 戻り値 : ID(after関数の設定情報に紐づくID)
         # self.master.after(5000, self.getHelloWorld, {
         #     "text" : "hello world",
         # })
@@ -21,7 +21,7 @@ class Application(tk.Frame):
         # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
         # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
         # 第三引数(任意) : 関数実行時に渡される引数
-        # 戻り値 : ID(after関数の設定情報に紐づく文字列)
+        # 戻り値 : ID(after関数の設定情報に紐づくID)
         return self.master.after(5000, self.getHelloWorld, {
             "text" : "hello world",
         })
@@ -44,9 +44,9 @@ class Application(tk.Frame):
         button.pack()
 
     # after関数をキャンセルする関数
-    # id : after関数の設定情報に紐づく文字列
+    # id : after関数の設定情報に紐づくID
     def removeAfterSample(self, id):
-        # 第一引数 : after関数の設定情報に紐づく文字列
+        # 第一引数 : after関数の設定情報に紐づくID
         self.master.after_cancel(id)
 
     def __init__(self, master=None):
