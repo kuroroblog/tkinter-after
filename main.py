f1571fe10ac172d63ff9fc71bd61ee303edbf4c2
tocchimittsu <arimagedon21@gmail.com>
2021-07-01 09:09:46 +0900

    修正

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Application(tk.Frame):
         # 5000ms後にself.getHelloWorld関数を実行
         # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
         # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
-        # 第三引数 : 関数実行時に渡される引数
+        # 第三引数(任意) : 関数実行時に渡される引数
         # 戻り値 : ID(after関数の設定情報に紐づく文字列)
         # self.master.after(5000, self.getHelloWorld, {
         #     "text" : "hello world",
@@ -20,7 +20,7 @@ class Application(tk.Frame):
         # 5000ms後にself.getHelloWorld関数を実行
         # 第一引数 : どのくらい遅らせて第二引数を実行するのかを時間設定する(単位はミリ秒)。
         # 第二引数 : 第一引数経過後に実行する関数。self.getHelloWorldとする。
-        # 第三引数 : 関数実行時に渡される引数
+        # 第三引数(任意) : 関数実行時に渡される引数
         # 戻り値 : ID(after関数の設定情報に紐づく文字列)
         return self.master.after(5000, self.getHelloWorld, {
             "text" : "hello world",
